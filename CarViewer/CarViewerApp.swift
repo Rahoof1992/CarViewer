d36e96c80b4615a7b2c9d67b026ac8d8d755015d
rahoofjamil <rahoof_1@live.de>
2023-03-03 16:14:07 +0100

    Initial Commit

---
+++ CarViewer/CarViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarViewerApp.swift
+//  CarViewer
+//
+//  Created by Rahoof Jamil on 03.03.23.
+//
+
+import SwiftUI
+
+@main
+struct CarViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
